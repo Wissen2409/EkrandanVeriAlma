300a9eda1a11e6769a9880e50d4d4eb185ad2f04
emrahelis <emrahelis@gmail.com>
2024-09-07 14:18:44 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// Ekrandan kullanıcıdan değer almak için, 
+//Console.ReadLine();
+
+
+using System.Runtime.Intrinsics.Arm;
+
+/*
+Console.WriteLine("Merhaba adınız nedir");
+string metin =Console.ReadLine();
+// Console.Readline fonksiyonundan geriye string değer döner, ekrandan aldığımız değer bu fonksiyon ile bize verilir.
+// Console.ReadLine içerisinde gelen metinsel değeri, bir değişken üzerine atıp, ekranda gösterebiliriz.
+Console.WriteLine("Hoşgeldin : {0}",metin);
+*/
+
+// Ekrandan iki adet sayı alıp birbiri ile toplayınız.
+Console.WriteLine("1. sayıyı giriniz");
+string birinciSayi =Console.ReadLine();
+Console.WriteLine("2. sayıyı giriniz");
+string ikinciSayi = Console.ReadLine();
+
+
+// metinsel ifadelerde + operatörü birleştirme işlemi yapar.
+Console.WriteLine(birinciSayi+ikinciSayi);
+
+
+// 
+
